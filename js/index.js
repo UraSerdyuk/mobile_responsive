ab29836d7cdfde0785e5e9d9b4d9301c796e18cf
Yura Serdiuk <cb8593bc@gmail.com>
2018-08-30 04:03:10 +0300

    add slider3

--- js/index.js
+++ js/index.js
@@ -1,7 +1,21 @@
 let logoMenu = document.getElementById('menu');
 let nav = document.getElementById('nav');
+let img = document.getElementById('slider');
+let imgSlider = 1;
 
+
+
+// Выпадающее меню
 logoMenu.addEventListener('click',function () {
     nav.classList.toggle("toogle");
     console.log('test');
 });
+//Slider
+setInterval(()=>{
+    imgSlider++;
+    if(imgSlider > 5){
+        imgSlider =1;
+    }
+    img.src =`./img/${imgSlider}.jpg`
+
+},5000)
