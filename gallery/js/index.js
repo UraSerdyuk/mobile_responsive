dbd18ff7b1d7300493e11f5714a068d69360b72b
Yura <cb8593bc@gmail.com>
2018-09-20 01:41:42 +0300

    add second gallery

--- gallery/js/index.js
+++ gallery/js/index.js
@@ -17,28 +17,49 @@ let active = document.getElementById('actived');
 let close = document.getElementById('closed');
 let link = document.getElementById('link');
 let img = document.getElementById('img480');
+let img2 = document.getElementById('img2_480');
 let arrowd = document.getElementById('arrowd');
 
 let num =1;
 if(num >= 9){
     num =9;
 }
+let num2=1;
+
 
 function Left() {
     if(num <=1){
         num =9;
     }
     num--;
-    img.src = `img/first/img${num}.jpg`;
+    img.src = `./img/first/mobile/img${num}.jpg`;
     console.log('show text left ');
 }
 
 function Right() {
     num++;
-    if(num > 9){
+    if(num > 8){
         num =1;
     }
-    img.src = `img/first/img${num}.jpg`;
+    img.src = `./img/first/mobile/img${num}.jpg`;
+    console.log('show text right ');
+}
+/*Second section*/
+function SecondLeft() {
+    if(num2 <=1){
+        num2 =8;
+    }
+    num2--;
+    img2.src = `./img/second/mobile/img${num2}.jpg`;
+    console.log('show text left ');
+}
+
+function SecondRight() {
+    num2++;
+    if(num2 > 8){
+        num2 =1;
+    }
+    img2.src = `./img/second/mobile/img${num2}.jpg`;
     console.log('show text right ');
 }
 
